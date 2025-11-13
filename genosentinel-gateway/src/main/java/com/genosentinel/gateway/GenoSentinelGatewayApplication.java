76a6e32ada73ba4ef0cd99c89b93af90a7865782
JuanCarvajal11 <carvajaljuanmanuel8@gmail.com>
2025-11-13 15:42:28 -0500

    Estructura inicial del projecto

---
+++ genosentinel-gateway/src/main/java/com/genosentinel/gateway/GenoSentinelGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.genosentinel.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenoSentinelGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenoSentinelGatewayApplication.class, args);
+	}
+
+}
